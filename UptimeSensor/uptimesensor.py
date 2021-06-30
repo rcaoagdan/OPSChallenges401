f9fe2f38c02c6e0a1139249e254091eaba3567ef
rcaoagdan <rcaoagdan@icloud.com>
2021-06-29 19:11:13 -0700

    update

--- UptimeSensor/uptimesensor.py
+++ UptimeSensor/uptimesensor.py
@@ -10,29 +10,24 @@
 ##############################################################################
 import os
 import datetime
+import subprocess
 
 ##############################################################################
 # Set Variable for a user input ip address
 ##############################################################################
-ip_input=input("Please enter in an IP Adress:")
-#ping_request=os.system("ping -w 2000 " + ip_input)
-ping_request=os.system("ping " + ip_input)
+ip_input=input("Please enter in an IP Address:")
+ping_request=os.system("ping -w 2000 " + ip_input)
 current_date_time=datetime.datetime.now()
-invalid=current_date_time , 'Network Not Active to', ip_input
-valid=current_date_time , 'Network Active to', ip_input
+print (" ")
 ##############################################################################
 # Ping Function
 ##############################################################################
-def ping_function():
-    if ping_request == 0:
-       print(current_date_time,"Network Active to",ip_input)
-    else:
+for ping in range (1,5):
+   if ping_request == 0: 
+      print(current_date_time,"Network Active to",ip_input) 
+   else:
       print(current_date_time,"Network NOT Active to",ip_input)
 
-##############################################################################
-# Main
-##############################################################################
 
-print(ping_request)
-print(" ")
-ping_function()
+
+
