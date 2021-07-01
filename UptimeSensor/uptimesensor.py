d94fedc032fbf4b60b9cf7a409cb1af9dd0ef387
rcaoagdan <rcaoagdan@icloud.com>
2021-06-30 20:48:02 -0700

    fix

--- UptimeSensor/uptimesensor.py
+++ UptimeSensor/uptimesensor.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
-#Script: Ops Challenge Uptime Sensor
-#Author: Ray Caoagdan
-#Date of Last Revision: 06/27/2021
-#Purpose: Uptime Sensor 
+# Script: Ops Challenge Uptime Sensor
+# Author: Ray Caoagdan
+# Date of Last Revision: 06/30/2021
+# Purpose: Uptime Sensor 
+# Part 1: Ping Status and Print to File
+# Part 2: Email Ping Status
 
 ##############################################################################
 # Import Library 
 ##############################################################################
 import os
 import datetime
-
+import getpass
+import email
+import smtplib # email sending 
+from email.message import EmailMessage #email module
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
 
 ##############################################################################
 # Set Variable for a user input ip address
@@ -21,18 +30,43 @@ current_date_time=datetime.datetime.now()
 dataFile=open("data.txt", "w")
 
 ##############################################################################
-# Ping Status Function, prints to results to file
+# Ping Status Function | Prints to results to file
 ##############################################################################
-def pingStatus():
+def PingStatus():
    for ping in range (0,4):
       if ping_request == 0: 
-         print(current_date_time,"Network Active to",ip_input, file=dataFile) 
+         print(current_date_time,"Network Active to",ip_input, file=dataFile)
+        
       else:
          print(current_date_time,"Network NOT Active to",ip_input,file=dataFile)
-   dataFile.close()
+     
 
+##############################################################################
+# Email Ping Status
+##############################################################################
+def mailPingStatus(): 
+   adminEmail=input('Enter Administrators Email:')
+   userEmail=input('Enter your Email:')
+   userPass=getpass.getpass("Password:")
+   mail=smtplib.SMTP_SSL("smtp.gmail.com", 465)
+   files=open("data.txt", "r")
+   emailMSG=MIMEMultipart()
+   emailMSG['From']=userEmail
+   emailMSG['To']=adminEmail
+   emailMSG['Subject']='Ping Status'
+   #email_body="Hello, attached are the results of the Ping Status"
+   emailMSG.attach(MIMEText(files.read(), "plain"))
+   files.close()
+   mail.ehlo()
+   mail.login(userEmail, userPass)
+   mail.sendmail(userEmail, adminEmail, emailMSG.as_string())
+   mail.quit()
 ##############################################################################
 # main
 ##############################################################################
 print(" ")
-pingStatus()
+PingStatus()
+mailPingStatus()
+##############################################################################
+# End
+##############################################################################
