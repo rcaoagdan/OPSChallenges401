8e760fd54c9e633b18737fa08e2ea62d56aa4039
rcaoagdan <rcaoagdan@icloud.com>
2021-07-27 19:13:47 -0700

    WIP

--- Network_Security_Tool/netsectool.py
+++ Network_Security_Tool/netsectool.py
@@ -19,22 +19,24 @@ from scapy.volatile import RandShort
 # Global Variables
 ##############################################################################
 dIP = input("Enter a Destination IP Address:") # Destination IP
-mPort = input('Enter Minimum:') # set minum
-MxPort = input('Enter Maximum Port:') # set max port range
+nPorts = int(input("Enter number of ports you wish to scan: "))
+rPort=list(map(int,input("\nEnter Ports: ").strip().split()))[:nPorts]
+# mPort = input('Enter Minimum:') # set minum
+# MxPort = input('Enter Maximum Port:') # set max port range
 srcPort = RandShort() # Generate small random source port number from scappy 
 SYNACK = 0X12
 RSTACK = 0X14
 ##############################################################################
 # Check validity of inputs
 ##############################################################################
-if int(mPort) >= 0 and int(MxPort) >= 0 and int(MxPort) >= int(mPort):
-        pass
-        rPorts = range(int(mPort),int(MxPort)+1)
-else:
-        print("Invalid Range!!! \n")
-        print("Learn some math \n")
-        print("Now Exiting.....")
-        sys.exit(1)
+#if int(mPort) >= 0 and int(MxPort) >= 0 and int(MxPort) >= int(mPort):
+        # pass
+       #  rPorts = range(int(mPort),int(MxPort)+1)
+# else:
+        # print("Invalid Range!!! \n")
+        # print("Learn some math \n")
+        # print("Now Exiting.....")
+        # sys.exit(1)
 ##############################################################################
 # End of Validity check
 ##############################################################################
@@ -45,21 +47,21 @@ else:
 # TCP Port open Closed
 ##############################################################################
 def tcpScan():
-    for dstPort in rPorts:
+    for dstPort in rPort:
         resp=sr1(IP(dst=dIP)/TCP(sport=srcPort,dport=dstPort,flags="S"),timeout=1,verbose=0)
     
         if resp is None:
             #@print(f"{dIP}:{rPorts} is filtered and silently dropped")
-            print("Port" + str(rPorts) + "is filtered and silently dropped")
+            print("Port" + str(rPort) + "is filtered and silently dropped")
         
         elif (resp.haslayer(TCP)):
             if(resp.getlayer(TCP).flags == SYNACK):
                 #send_rst= sr1(IP(dst=dIP)/TCP(sport=srcPort,dport=dstPort,flags="R"),timeout=1,verbose=0)
                 #print(f"{dIP}:{rPorts} is open")
-                print("Port" + str(rPorts) + " is open")
+                print("Port" + str(rPort) + " is open")
 
             elif(resp.getlayer(TCP).flags == RSTACK):
-                print(f"{dIP}:{rPorts} is closed")
+                print(f"{dIP}:{rPort} is closed")
             
             else:
                 print("ERROR TCP SCAN")
