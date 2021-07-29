0b89b84c0ba09b5ba19908a57c0e34a24be7a1ab
rcaoagdan <rcaoagdan@icloud.com>
2021-07-28 18:59:13 -0700

    figured it out

--- Network_Security_Tool/netsectool.py
+++ Network_Security_Tool/netsectool.py
@@ -12,72 +12,84 @@
 ##############################################################################
 import random
 import sys
-from scapy.all import ICMP,IP,sr1,TCP
+from typing import List
+from scapy.all import *
 from scapy.volatile import RandShort
 from datetime import datetime
 
 ##############################################################################
+# Main Menu
+##############################################################################
+def main_menu():
+    print("*" * 50)
+    print("What Would You Like to Do?")
+    print("1.TCP Port Scanner")
+    print("2.ICMP Ping Sweep")
+    print("3.Exit")
+    print("*" * 50)
+    mainInput= input("OPTION: ")
+    print("*" * 50)
+    if mainInput == '1':
+        tcpScan()
+    elif mainInput ==  '2':
+        ICMPSweep()
+    elif mainInput == '3':
+        exit
+    else:
+        print("Incorrect Selection")
+        main_menu
+##############################################################################
 # Global Variables
 ##############################################################################
-dIP = input("Enter a Destination IP Address:") # Destination IP
-nPorts = int(input("Enter number of ports you wish to scan: "))
-rPort=list(map(int,input("\nEnter Ports: ").strip().split()))[:nPorts]
-# mPort = input('Enter Minimum:') # set minum
-# MxPort = input('Enter Maximum Port:') # set max port range
-srcPort = RandShort() # Generate small random source port number from scappy 
-SYNACK = 0X12
-RSTACK = 0X14
-##############################################################################
-# Check validity of inputs
-##############################################################################
-#if int(mPort) >= 0 and int(MxPort) >= 0 and int(MxPort) >= int(mPort):
-        # pass
-       #  rPorts = range(int(mPort),int(MxPort)+1)
-# else:
-        # print("Invalid Range!!! \n")
-        # print("Learn some math \n")
-        # print("Now Exiting.....")
-        # sys.exit(1)
-##############################################################################
-# End of Validity check
-##############################################################################
-
 
 
 ##############################################################################
 # TCP Port open Closed
 ##############################################################################
 def tcpScan():
-    print("*" * 50)
+    dIP = input("Enter a Destination IP Address:")
+    nPorts = int(input("Enter number of ports you wish to scan: "))
+    rPort=list(map(int,input("\nEnter Ports: ").strip().split()))[:nPorts] 
+    srcPort = random.randint(1,5000) 
+    SYNACK = 0X12 # Var for SYNACK flag
+    RSTACK = 0X14 # Var fpr RSTACK flag
+    print("*" * 50) # prints * 50 times
     print("Scanning:" + dIP )
-    print("Scanning started at:" + str(datetime.now()))
+    print("Scanning started at: " + str(datetime.now()))
     print("*" * 50)
+
     for dstPort in rPort:
         resp=sr1(IP(dst=dIP)/TCP(sport=srcPort,dport=dstPort,flags="S"),timeout=1,verbose=0)
     
         if resp is None:
-            #@print(f"{dIP}:{rPorts} is filtered and silently dropped")
-            print("Port" + str(rPort) + "is filtered and silently dropped")
+
+            print("Port " + str(dstPort) + " is filtered and silently dropped")
         
         elif (resp.haslayer(TCP)):
             if(resp.getlayer(TCP).flags == SYNACK):
-                #send_rst= sr1(IP(dst=dIP)/TCP(sport=srcPort,dport=dstPort,flags="R"),timeout=1,verbose=0)
-                #print(f"{dIP}:{rPorts} is open")
-                print("Port" + str(rPort) + " is open")
+              
+                print("Port " + str(dstPort) + " is open")
 
             elif(resp.getlayer(TCP).flags == RSTACK):
-                print(f"{dIP}:{rPort} is closed")
+                print("Port " + str(dstPort) + " is closed")
             
             else:
                 print("ERROR TCP SCAN")
 
         else:
             print("ERROR SCANNING")
+    main_menu()
 
+##############################################################################
+# ICMP Sweep
+##############################################################################
+def ICMPSweep():
+    print("WIP")
+    main_menu()
 ##############################################################################
 # Main
 ##############################################################################
-tcpScan()
+main_menu()
 ##############################################################################
 # End
 ##############################################################################
