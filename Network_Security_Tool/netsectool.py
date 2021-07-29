a9f0720029e607a1ba0cfaa7757bfde829abb4e0
rcaoagdan <rcaoagdan@icloud.com>
2021-07-28 19:11:45 -0700

    part 1 compolete

--- Network_Security_Tool/netsectool.py
+++ Network_Security_Tool/netsectool.py
@@ -2,7 +2,7 @@
 
 # Script: Network Security Tool
 # Author: Ray Caoagdan
-# Date of Last Revision: 
+# Date of Last Revision: 07/28/2021
 # Purpose:  Scan Ports
 
 
@@ -14,7 +14,6 @@ import random
 import sys
 from typing import List
 from scapy.all import *
-from scapy.volatile import RandShort
 from datetime import datetime
 
 ##############################################################################
@@ -27,8 +26,8 @@ def main_menu():
     print("2.ICMP Ping Sweep")
     print("3.Exit")
     print("*" * 50)
-    mainInput= input("OPTION: ")
-    print("*" * 50)
+    mainInput= input("OPTION: " ) 
+    print("*" * 50 + "\n")
     if mainInput == '1':
         tcpScan()
     elif mainInput ==  '2':
@@ -38,13 +37,11 @@ def main_menu():
     else:
         print("Incorrect Selection")
         main_menu
-##############################################################################
-# Global Variables
-##############################################################################
+
 
 
 ##############################################################################
-# TCP Port open Closed
+# TCP Port Scanner
 ##############################################################################
 def tcpScan():
     dIP = input("Enter a Destination IP Address:")
@@ -53,31 +50,36 @@ def tcpScan():
     srcPort = random.randint(1,5000) 
     SYNACK = 0X12 # Var for SYNACK flag
     RSTACK = 0X14 # Var fpr RSTACK flag
-    print("*" * 50) # prints * 50 times
+
+    print("*" * 50) 
     print("Scanning:" + dIP )
     print("Scanning started at: " + str(datetime.now()))
     print("*" * 50)
+    print (" ")
 
     for dstPort in rPort:
         resp=sr1(IP(dst=dIP)/TCP(sport=srcPort,dport=dstPort,flags="S"),timeout=1,verbose=0)
     
         if resp is None:
-
             print("Port " + str(dstPort) + " is filtered and silently dropped")
         
         elif (resp.haslayer(TCP)):
             if(resp.getlayer(TCP).flags == SYNACK):
-              
                 print("Port " + str(dstPort) + " is open")
 
             elif(resp.getlayer(TCP).flags == RSTACK):
                 print("Port " + str(dstPort) + " is closed")
-            
+
             else:
                 print("ERROR TCP SCAN")
 
         else:
             print("ERROR SCANNING")
+    print(" ")
+    print("*" * 50) 
+    print("Scanned:" + dIP )
+    print("Scan Ended at: " + str(datetime.now()))
+    print("*" * 50 + "\n") 
     main_menu()
 
 ##############################################################################
