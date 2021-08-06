949eb204306c0afc3813d07427fd6abeb6c5b930
rcaoagdan <rcaoagdan@icloud.com>
2021-08-05 17:39:49 -0700

    clean up

---
+++ Network_Security_Tool/NMAPTOOL.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+# Script: Network Security Tool-NMAP
+# Author: Ray Caoagdan
+# Date of Last Revision: 08/05/2021
+
+##############################################################################
+# Import Library
+##############################################################################
+import os
+import nmap
+from datetime import datetime
+
+
+
+##############################################################################
+# Ping and Port Scanner with Nmap
+##############################################################################
+def nmapTool():
+    print(" ")
+    print("*" * 50) 
+    print("Ping and Port Scanner \n")
+    print("*" * 50) 
+    print(" ")
+
+    target_IP = input("Please enter an IP to Ping: ")
+    ping_IP=os.system("ping -w 2000 -c 1 " + target_IP)
+    IPscan=nmap.PortScanner()
+
+    if ping_IP == 0:
+        print(" ")
+        print("*" * 50) 
+        print("Network is up")
+        print("We Will Now Begin Scanning for Open Ports")
+        print("*" * 50) 
+        print(" ")
+        bPort = int(input("Enter begining port: "))
+        ePort = int(input("Enter ending port: "))
+        print(" ")
+        print("*" * 50) 
+        print("Scaning Ports " + str(bPort) + "-" + str(ePort))
+        print("*" * 50) 
+        print(" ")
+        for port in range(bPort,ePort+1):
+            res = IPscan.scan(target_IP,str(port))
+            res = res['scan'][target_IP]['tcp'][port]['state']# target results in dictionary
+            print(f'port {port} is {res}')
+        print(" ")
+        print("*" * 50) 
+        print("Pinged:" + target_IP )
+        print("Ports Scan " + str(bPort) + "-" + str(ePort))
+        print("Ping and Scan Ended at: " + str(datetime.now()))
+        print("*" * 50 + "\n")  
+
+    else:
+        print(" ")
+        print("*" * 50) 
+        print("Pinged:" + target_IP )
+        print("Network is not Active")
+        print("Ping Ended at: " + str(datetime.now()))
+        print("*" * 50 + "\n")  
+
+    newScan()
+    
+def newScan():
+    continueScan=input("Do you want to do another scan Y/N? ")
+
+    if continueScan == "Y" or continueScan == "y":
+        nmapTool()
+    elif continueScan == "N" or continueScan == "n":
+        exit
+    else:
+        print("Incorrect Selection.... Please make a correct selection \n")
+        newScan()
+
+nmapTool()
+
+##############################################################################
+# Source
+##############################################################################
+
+# geeksforgeeks.org/port-scanner-using-python-nmap/
--- Network_Security_Tool/netsectool.py
+++ Network_Security_Tool/netsectool.py
@@ -3,102 +3,40 @@
 # Script: Network Security Tool
 # Author: Ray Caoagdan
 # Date of Last Revision: 08/04/2021
-# Purpose:  Scan Ports | ICMP Sweep | Combined 
+# Purpose:  Scan Ports | ICMP Sweep 
 
 ##############################################################################
 # Import Library 
 ##############################################################################
 from re import VERBOSE
 from typing import List
-import os
 from uuid import NAMESPACE_OID
-import nmap
 from ipaddress import IPv4Network
 from scapy.all import *
 from datetime import datetime
-
 from scapy.modules.six import u
 
-
-##############################################################################
-# Ping and Port Scanner with Nmap
-##############################################################################
-
-print(" ")
-print("*" * 50) 
-print("Ping and Port Scanner \n")
-print("*" * 50) 
-print(" ")
-
-target_IP = input("Please enter an IP to Ping: ")
-ping_IP=os.system("ping -w 2000 -c 1 " + target_IP)
-IPscan=nmap.PortScanner()
-
-if ping_IP == 0:
-    print(" ")
-    print("*" * 50) 
-    print("Network is up")
-    print("We Will Now Begin Scanning for Open Ports")
-    print("*" * 50) 
-    print(" ")
-    bPort = int(input("Enter begining port: "))
-    ePort = int(input("Enter ending port: "))
-    print(" ")
-    print("*" * 50) 
-    print("Scaning Ports " + str(bPort) + "-" + str(ePort))
-    print("*" * 50) 
-    print(" ")
-    for i in range(bPort,ePort+1):
-        res = IPscan.scan(target_IP,str(i))
-        res = res['scan'][target_IP]['tcp'][i]['state']# target results in dictionary
-        print(f'port {i} is {res}')
-    print(" ")
-    print("*" * 50) 
-    print("Pinged:" + target_IP )
-    print("Ports Scan " + str(bPort) + "-" + str(ePort))
-    print("Ping and Scan Ended at: " + str(datetime.now()))
-    print("*" * 50 + "\n")  
-
-else:
-    print(" ")
-    print("*" * 50) 
-    print("Pinged:" + target_IP )
-    print("Network is not Active")
-    print("Ping Ended at: " + str(datetime.now()))
-    print("*" * 50 + "\n")  
-
-
-
-##############################################################################
-# Original Code
-##############################################################################
-#
-# Below is the Original Code for parts 1 and 2. 
-# Part 3 combined parts 1 and 2 into one code. 
-#
-#
-#
 ##############################################################################
 # Main Menu
 ##############################################################################
-# def main_menu():
-#     print("*" * 50)
-#     print("What Would You Like to Do?")
-#     print("1.TCP Port Scanner")
-#     print("2.ICMP Ping Sweep")
-#     print("3.Exit)
-#     print("*" * 50)
-#     mainInput= input("OPTION: " ) 
-#     print("*" * 50 + "\n")
-#     if mainInput == '1':
-#         tcpScan()
-#     elif mainInput ==  '2':
-#         ICMPSweep()
-#     elif mainInput == '3':
-#         exit
-#     else:
-#         print("Incorrect Selection")
-#         main_menu
+def main_menu():
+    print("*" * 50)
+    print("What Would You Like to Do?")
+    print("1.TCP Port Scanner")
+    print("2.ICMP Ping Sweep")
+    print("3.Exit)
+    print("*" * 50)
+    mainInput= input("OPTION: " ) 
+    print("*" * 50 + "\n")
+    if mainInput == '1':
+        tcpScan()
+    elif mainInput ==  '2':
+        ICMPSweep()
+    elif mainInput == '3':
+        exit
+    else:
+        print("Incorrect Selection")
+        main_menu
 
 
 
@@ -106,104 +44,102 @@ else:
 ##############################################################################
 # TCP Port Scanner
 ##############################################################################
-# def tcpScan():
-#     print(" ")
-#     print("*" * 50) 
-#     print("TCP Port Scanner \n")
-#     print("*" * 50) 
-#     print(" ")
+def tcpScan():
+    print(" ")
+    print("*" * 50) 
+    print("TCP Port Scanner \n")
+    print("*" * 50) 
+    print(" ")
 
-#     dIP = input("Enter a Destination IP Address:")
-#     nPorts = int(input("Enter number of ports you wish to scan: "))
-#     rPort=list(map(int,input("\nEnter Ports: ").strip().split()))[:nPorts] 
-#     srcPort = random.randint(1000,50000) 
-#     SYNACK = 0X12 # Var for SYNACK flag
-#     RSTACK = 0X14 # Var fpr RSTACK flag
+    dIP = input("Enter a Destination IP Address:")
+    nPorts = int(input("Enter number of ports you wish to scan: "))
+    rPort=list(map(int,input("\nEnter Ports: ").strip().split()))[:nPorts] 
+    srcPort = random.randint(1000,50000) 
+    SYNACK = 0X12 # Var for SYNACK flag
+    RSTACK = 0X14 # Var fpr RSTACK flag
 
-#     print("*" * 50) 
-#     print("Scanning:" + dIP )
-#     print("Scanning started at: " + str(datetime.now()))
-#     print("*" * 50)
-#     print (" ")
+    print("*" * 50) 
+    print("Scanning:" + dIP )
+    print("Scanning started at: " + str(datetime.now()))
+    print("*" * 50)
+    print (" ")
 
-#     for dstPort in rPort:
-#         resp=sr1(IP(dst=dIP)/TCP(sport=srcPort,dport=dstPort,flags="S"),timeout=1,verbose=0)
+    for dstPort in rPort:
+        resp=sr1(IP(dst=dIP)/TCP(sport=srcPort,dport=dstPort,flags="S"),timeout=1,verbose=0)
     
-#         if resp is None:
-#             print("Port " + str(dstPort) + " is filtered and silently dropped")
+        if resp is None:
+            print("Port " + str(dstPort) + " is filtered and silently dropped")
         
-#         elif (resp.haslayer(TCP)):
-#             if(resp.getlayer(TCP).flags == SYNACK):
-#                 print("Port " + str(dstPort) + " is open")
+        elif (resp.haslayer(TCP)):
+            if(resp.getlayer(TCP).flags == SYNACK):
+                print("Port " + str(dstPort) + " is open")
 
-#             elif(resp.getlayer(TCP).flags == RSTACK):
-#                 print("Port " + str(dstPort) + " is closed")
+            elif(resp.getlayer(TCP).flags == RSTACK):
+                print("Port " + str(dstPort) + " is closed")
 
-#             else:
-#                 print("ERROR TCP SCAN")
+            else:
+                print("ERROR TCP SCAN")
 
-#         else:
-#             print("ERROR SCANNING")
-#     print(" ")
-#     print("*" * 50) 
-#     print("Scanned:" + dIP )
-#     print("Scan Ended at: " + str(datetime.now()))
-#     print("*" * 50 + "\n") 
-#     main_menu()
+        else:
+            print("ERROR SCANNING")
+    print(" ")
+    print("*" * 50) 
+    print("Scanned:" + dIP )
+    print("Scan Ended at: " + str(datetime.now()))
+    print("*" * 50 + "\n") 
+    main_menu()
 
 ##############################################################################
 # ICMP Sweep
 ##############################################################################
-# def ICMPSweep():
-#     print(" ")
-#     print("*" * 50) 
-#     print("ICMP PING SWEEP")
-#     print("*" * 50) 
-#     print(" ")
+def ICMPSweep():
+    print(" ")
+    print("*" * 50) 
+    print("ICMP PING SWEEP")
+    print("*" * 50) 
+    print(" ")
     
     
-#     netIP=input("Input IP range to ping in format 0.0.0.0/0: ")
-#     netAddresses=IPv4Network(netIP)
-#     liveCount=0
+    netIP=input("Input IP range to ping in format 0.0.0.0/0: ")
+    netAddresses=IPv4Network(netIP)
+    liveCount=0
 
-#     print("*" * 50) 
-#     print("Pinging:" + netIP )
-#     print("Ping Sweep started at: " + str(datetime.now()))
-#     print("*" * 50)
-#     print (" ")
+    print("*" * 50) 
+    print("Pinging:" + netIP )
+    print("Ping Sweep started at: " + str(datetime.now()))
+    print("*" * 50)
+    print (" ")
 
-#     for host in netAddresses:
-#         if (host in (netAddresses.network_address, netAddresses.broadcast_address)):
-#             continue
-#         resp= sr1(IP(dst=str(host))/ICMP(),timeout=2,verbose=0)
+    for host in netAddresses:
+        if (host in (netAddresses.network_address, netAddresses.broadcast_address)):
+            continue
+        resp= sr1(IP(dst=str(host))/ICMP(),timeout=2,verbose=0)
 
-#         if resp is None:
-#             print (str(host) + " is down/unresponsive.")
-#         elif(int(resp.getlayer(ICMP).type) == 3 and int(resp.getlayer(ICMP).code) in [1,2,3,9,10,13]):
-#             print(str(host)  + " is blocking ICMP traffic")
-#         else:
-#             print(str(host)  + " is responding.")
-#             liveCount += 1
+        if resp is None:
+            print (str(host) + " is down/unresponsive.")
+        elif(int(resp.getlayer(ICMP).type) == 3 and int(resp.getlayer(ICMP).code) in [1,2,3,9,10,13]):
+            print(str(host)  + " is blocking ICMP traffic")
+        else:
+            print(str(host)  + " is responding.")
+            liveCount += 1
         
-#     print(liveCount/netAddresses.num_addresses + " hosts are online")
+    print(liveCount/netAddresses.num_addresses + " hosts are online")
 
-#     print (" ")
-#     print("*" * 50) 
-#     print("Pinged:" + netIP )
-#     print("Ping Sweep ended at: " + str(datetime.now()))
-#     print("*" * 50)
-#     print (" ")
-#     main_menu()
+    print (" ")
+    print("*" * 50) 
+    print("Pinged:" + netIP )
+    print("Ping Sweep ended at: " + str(datetime.now()))
+    print("*" * 50)
+    print (" ")
+    main_menu()
 
 
 ##############################################################################
 # Main
 ##############################################################################
-#main_menu()
+main_menu()
 
-##############################################################################
-# End of Original
-##############################################################################
+
 
 ##############################################################################
 # Source/ Refrence
@@ -218,8 +154,6 @@ else:
 ##ICPMP Ping Sweep##
 # https://thepacketgeek.com/scapy/building-network-tools/part-10/
 
-## NMAP ##
-# 
 #### For your Reference ####
 
 ### TCP SCAN ###
