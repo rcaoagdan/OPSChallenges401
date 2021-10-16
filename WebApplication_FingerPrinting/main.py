47e02903a289a434e8e24777c87f8702479192d1
rcaoagdan <rcaoagdan@icloud.com>
2021-10-16 12:18:14 -0700

    framework

---
+++ WebApplication_FingerPrinting/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/evn python3
+
+##############################################################################
+# Information
+##############################################################################
+
+# Script : Ops Challenge  Web Application Fingerprinting
+# Author : Ray Caoagdan
+# Date of Last Revision : 10.16.21
+# Purpose : Web Application Fingerpringiting | telnet , netcat , nmap
