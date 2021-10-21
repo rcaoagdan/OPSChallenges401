578fa2f5465f22d8675b9e136c67856bf754a507
rcaoagdan <rcaoagdan@icloud.com>
2021-10-20 18:54:36 -0700

    cookies

--- WebApplication_FingerPrinting/cookies.py
+++ WebApplication_FingerPrinting/cookies.py
@@ -4,6 +4,7 @@
 
 import requests
 import webbrowser
+import os
 
 #targetsite = input("Enter target site:") # Uncomment this to accept user input target site
 targetsite = "http://www.whatarecookies.com/cookietest.asp" # Comment this out if you're using the line above
@@ -25,11 +26,13 @@ def bringforthcookiemonster(): # Because why not!
       `. "    '"--...--"'  . ' .'  .'  o   `.
 
         ''')
+
 bringforthcookiemonster()
 print("Target site is " + targetsite)
 print(" ")
 print(cookie)
 print(" ")
+
 # - Send the cookie back to the site and receive a HTTP response
 print("*" * 75)
 response2 = requests.get(targetsite, cookies=cookie)
@@ -37,20 +40,22 @@ print("Returning the cookie back to " + targetsite + "\n")
 print("The HTTP RESPONSE IS....")
 print(response2)
 print(" ")
+
 # - Generate a .html file to capture the contents of the HTTP response
 print("*" * 75)
 print("Saving reponse.......")
-html_file = 'httpFile.hmtl'
+html_file = 'httpFile.html'
 file = requests.get(targetsite)
 with open(html_file, 'wb') as f:
   f.write(file.content)
 openClose = input ("Shall we open the file Y/N?")
 if (openClose == "Y" or "y"):
-  # - Open it with browser of your choosing
-  webbrowser.open_new_tab(html_file)
+
+  # - Open it with  default browser 
+  webbrowser.open_new_tab(html_file) 
 
 elif(openClose == "N" or "n"):
-  print("COOKIE MONSTER SAYS BYE BYE!")
+  print("COOKIE MONSTER SAYS BYE BYE!")  
   exit
 else:
   print("COOKIE MONSTER SAD YOU DIDN'T ENTER A CORRECT INPUT")
