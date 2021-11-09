5d5a62829a3366e3e218929c772b43461b91c08d
rcaoagdan <rcaoagdan@icloud.com>
2021-11-08 21:58:00 -0800

    SCAN

--- atk_tools/nmapScan.py
+++ atk_tools/nmapScan.py
@@ -18,29 +18,36 @@ print("--------------------")
 ip_addr = input("IP address to scan: ")
 print("The IP you entered is: ", ip_addr)
 type(ip_addr)
-bport = input("Enter a staring port: ")
-eport = input("Enter an ending port: ")
+bport = int(input("Enter a staring port: "))
+eport = int(input("Enter an ending port: "))
 resp = input("""\nSelect scan to execute:
                 1) SYN ACK Scan
                 2) UDP Scan
-                3)              \n""") ### TODO: Select what your third scan type will be
+                3) O/S Detection            \n""") 
 print("You have selected option: ", resp + " \n")
-range= (bport-eport)
 
-### TODO: Prompt the user to type in a port range for this tool to scan
+
+
 
 if resp == '1':
     print("Nmap Version: ", scanner.nmap_version())
-    scanner.scan(ip_addr, str(range), '-v -sS')
-    print(scanner.scaninfo())
-    print("Ip Status: ", scanner[ip_addr].state())
-    print(scanner[ip_addr].all_protocols())
-    print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
+    for port in range(bport,eport+1):
+        scanner.scan(ip_addr, str(port), '-v -sS')
+        print(scanner.scaninfo())
+        print("Ip Status: ", scanner[ip_addr].state())
+        print(scanner[ip_addr].all_protocols())
+        print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
 elif resp == '2':
-    ### TODO: Add missing code block here
-    print("Please enter a valid option") 
+    print("Nmap Version: ", scanner.nmap_version())
+    for port in range(bport,eport+1):
+        scanner.scan(ip_addr, str(port), '-v -sU')
+        print(scanner.scaninfo())
+        print("Ip Status: ", scanner[ip_addr].state())
+        print(scanner[ip_addr].all_protocols())
+        print("Open Ports: ", scanner[ip_addr]['udp'].keys())
 elif resp == '3':
-    ### TODO: Add missing code block here
-    print("Please enter a valid option") #
-elif resp >= '4':
+    print("Determining Target's O/S")
+    target_machine = scanner.scan(ip_addr, arguments='-O')
+    print(target_machine['scan'][ip_addr]['osmatch'][0]['osclass'][0]['osfamily'])
+else:
     print("Please enter a valid option")
